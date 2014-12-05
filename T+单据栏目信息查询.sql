589629274ebc77fa644134d56dfa7e807c8116d4
pengmin <moguipm@sina.com>
2014-12-05 18:07:35 +0800

    更新单据信息查询sql

--- T+单据栏目信息查询.sql
+++ T+单据栏目信息查询.sql
@@ -13,14 +13,28 @@ JOIN dbo.eap_DTO AS c ON c.Name=b.DtoName
 WHERE c.TableName='SA_SaleOrder' AND a.title LIKE '%业务类型%'
 ORDER BY a.TabIndex
 -------------------------------------------------------------
-SELECT * FROM dbo.eap_Voucher WHERE DtoName='SA_SaleOrder'
-SELECT * FROM dbo.eap_DTO WHERE TableName='SA_SaleOrder'
-SELECT * FROM dbo.eap_DTOProperty WHERE idDTO='38BC85B9-E6BC-4A06-9BFC-D1D60B86B752' AND FieldName LIKE '%idbu%'
-SELECT * FROM dbo.eap_EditTable WHERE DtoName='SaleOrderDTO'
-SELECT * FROM dbo.eap_DTORelation WHERE idProperty='C2C24F05-E887-4D12-A961-B223805974DA'
-
-SELECT * FROM dbo.eap_VoucherControls WHERE title LIKE '%业务类型%' AND VoucherID='F47B4F5D-3916-48EF-AB08-879773C6426F'
-
-SELECT * FROM dbo.eap_Enum WHERE id='972BFAC1-8DB8-4F54-A0F6-CA5878FAB250'
-SELECT * FROM dbo.eap_EnumItem WHERE Name LIKE '%采购%'
-SELECT * FROM enum
+--以下查询dto之间的关系
+SELECT * FROM dbo.eap_DTO WHERE TableName='PU_PurchaseOrder'
+SELECT * FROM dbo.eap_DTOProperty WHERE idDTO='DA8CB4FD-CA9A-427F-99AF-4FC636CD087C'
+SELECT * FROM dbo.eap_DTORelation
+SELECT * FROM dbo.EAP_UserDefineArticleDTO_0001
+SELECT * FROM dbo.EAP_UserDefineArticleDTO_0002
+SELECT * FROM dbo.EAP_UserDefineArticleDTO_0003
+--dto引用的其他dto字段
+SELECT a.id,b.Title,c.Title,d.FieldName,e.FieldName,f.FieldName,a.* 
+FROM dbo.eap_DTORelation AS a
+JOIN dbo.eap_DTO AS b ON a.idFromDTO=b.ID
+JOIN dbo.eap_DTO AS c ON a.idToDTO=c.ID
+JOIN dbo.eap_DTOProperty AS d ON a.idProperty=d.ID
+JOIN dbo.eap_DTOProperty AS e ON a.idFromProperty=e.ID
+JOIN dbo.eap_DTOProperty AS f ON a.idToProperty=f.ID
+WHERE idFromDTO='DA8CB4FD-CA9A-427F-99AF-4FC636CD087C'
+--被其他dto字段引用的dto
+SELECT a.id,b.Title,c.Title,d.FieldName,e.FieldName,f.FieldName,a.* 
+FROM dbo.eap_DTORelation AS a
+JOIN dbo.eap_DTO AS b ON a.idFromDTO=b.ID
+JOIN dbo.eap_DTO AS c ON a.idToDTO=c.ID
+JOIN dbo.eap_DTOProperty AS d ON a.idProperty=d.ID
+JOIN dbo.eap_DTOProperty AS e ON a.idFromProperty=e.ID
+JOIN dbo.eap_DTOProperty AS f ON a.idToProperty=f.ID
+WHERE a.idToDTO='DA8CB4FD-CA9A-427F-99AF-4FC636CD087C'
