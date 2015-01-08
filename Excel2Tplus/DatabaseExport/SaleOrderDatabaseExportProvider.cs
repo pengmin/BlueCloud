311971052bb9244f404c64106a3666b47d005834
pengmin <2261736011@qq.com>
2015-01-08 22:15:24 +0800

    修改天邦Bug

--- Excel2Tplus/DatabaseExport/SaleOrderDatabaseExportProvider.cs
+++ Excel2Tplus/DatabaseExport/SaleOrderDatabaseExportProvider.cs
@@ -161,7 +161,7 @@ namespace Excel2Tplus.DatabaseExport
 			{
 				list.Add("单据[" + obj.单据编号 + "]客户不存在");
 			}
-			if (TplusDatabaseHelper.Instance.GetParnerTypeByName(obj.客户) != "客户")
+			if (!TplusDatabaseHelper.Instance.GetParnerTypeByName(obj.客户).Contains("客户"))
 			{
 				list.Add("单据[" + obj.单据编号 + "](" + obj.客户 + ")不是客户性质的往来单位");
 			}
