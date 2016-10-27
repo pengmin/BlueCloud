b83c3d40f734e6c9d7db76bba0d26568ae095486
peng min <2261736011@qq.com>
2016-10-27 21:56:41 +0800

    修改报表sql

--- PrintService/SaleDeliveryPrint.aspx.cs
+++ PrintService/SaleDeliveryPrint.aspx.cs
@@ -52,30 +52,30 @@ GROUP BY temp.code,temp.name,temp.address,name1,temp.maker,temp.createdtime,temp
 	specification AS cln2,freeItem0 AS cln3, name AS cln4,
 	[28]+[29]+[30]+[31]+[32]+[33]+[34]+[35]+[36]+[37]+[38]+[39]+[40] AS cln5,
 	[28] AS cln6,[29] AS cln7,[30] AS cln8,[31] AS cln9,[32] AS cln10,[33] AS cln11,[34] AS cln12,
-	[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18
+	[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18,memo AS cln19
 FROM(
 	SELECT
-		specification,freeItem0,name,
+		specification,freeItem0,name,memo,
 		SUM([28]) AS [28],SUM([29]) AS [29],SUM([30]) AS [30],SUM([31]) AS [31],SUM([32]) AS [32],SUM([33]) AS [33],SUM([34]) AS [34],
 		SUM([35]) AS [35],SUM([36]) AS [36],SUM([37]) AS [37],SUM([38]) AS [38],SUM([39]) AS [39],SUM([40]) AS [40]
 	FROM(
 		SELECT
-			specification,freeItem0,name,
-			(CASE WHEN freeItem1='28#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [28],
-			(CASE WHEN freeItem1='29#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [29],
-			(CASE WHEN freeItem1='30#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [30],
-			(CASE WHEN freeItem1='31#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [31],
-			(CASE WHEN freeItem1='32#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [32],
-			(CASE WHEN freeItem1='33#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [33],
-			(CASE WHEN freeItem1='34#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [34],
-			(CASE WHEN freeItem1='35#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [35],
-			(CASE WHEN freeItem1='36#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [36],
-			(CASE WHEN freeItem1='37#' THEN quantity ELSE 0 END) AS [37],
-			(CASE WHEN freeItem1='38#' THEN quantity ELSE 0 END) AS [38],
-			(CASE WHEN freeItem1='39#' THEN quantity ELSE 0 END) AS [39],
+			specification,freeItem0,name,memo,
+			(CASE WHEN freeItem1='28#' OR freeItem1='XS' THEN quantity ELSE 0 END) AS [28],
+			(CASE WHEN freeItem1='29#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [29],
+			(CASE WHEN freeItem1='30#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [30],
+			(CASE WHEN freeItem1='31#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [31],
+			(CASE WHEN freeItem1='32#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [32],
+			(CASE WHEN freeItem1='33#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [33],
+			(CASE WHEN freeItem1='34#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [34],
+			(CASE WHEN freeItem1='35#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [35],
+			(CASE WHEN freeItem1='36#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [36],
+			(CASE WHEN freeItem1='37#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [37],
+			(CASE WHEN freeItem1='38#' OR freeItem1='7XL' THEN quantity ELSE 0 END) AS [38],
+			(CASE WHEN freeItem1='39#' OR freeItem1='8XL' THEN quantity ELSE 0 END) AS [39],
 			(CASE WHEN freeItem1='40#' THEN quantity ELSE 0 END) AS [40]
 		FROM(
-			select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity
+			select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity,MAX(d.memo) as memo
 			from SA_SaleDelivery_b as a 
 			left join aa_unit as b on a.idunit=b.id
 			left join aa_inventory as c on a.idinventory=c.id
@@ -83,9 +83,9 @@ FROM(
 			WHERE d.code='{0}'
 			GROUP BY c.specification,freeItem0,freeItem1,b.name
 		) AS temp
-		GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity
+		GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.memo
 	) AS temp
-	GROUP BY temp.specification, temp.freeItem0,temp.name
+	GROUP BY temp.specification, temp.freeItem0,temp.name,temp.memo
 ) AS temp";
 			return string.Format(sql, Request["code"]);
 		}
@@ -97,29 +97,29 @@ FROM(
 	[25]+[26]+[27]+[28]+[29]+[30]+[31]+[32]+[33]+[34]+[35]+[36]+[37]+[38]+[39]+[40]+[41]+[42]+[43]+[44]+[45] AS cln5,
 	[28] AS cln6,[29] AS cln7,[30] AS cln8,[31] AS cln9,[32] AS cln10,[33] AS cln11,[34] AS cln12,
 	[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18,
-	[25] AS cln19,[26] AS cln20,[27] AS cln21,[41] AS cln22,[42] AS cln23,[43] AS cln24,[44] AS cln25,[45] AS cln26
+	[25] AS cln19,[26] AS cln20,[27] AS cln21,[41] AS cln22,[42] AS cln23,[43] AS cln24,[44] AS cln25,[45] AS cln26,memo AS cln27
 FROM(
 	SELECT
-		specification,freeItem0,name,
+		specification,freeItem0,name,memo,
 		SUM([25]) AS [25],SUM([26]) AS [26],SUM([27]) AS [27],
 		SUM([28]) AS [28],SUM([29]) AS [29],SUM([30]) AS [30],SUM([31]) AS [31],SUM([32]) AS [32],SUM([33]) AS [33],SUM([34]) AS [34],
 		SUM([35]) AS [35],SUM([36]) AS [36],SUM([37]) AS [37],SUM([38]) AS [38],SUM([39]) AS [39],SUM([40]) AS [40],
 		SUM([41]) AS [41],SUM([42]) AS [42],SUM([43]) AS [43],SUM([44]) AS [44],SUM([45]) AS [45]
 	FROM(
 		SELECT
-			specification,freeItem0,name,
-			(CASE WHEN freeItem1='25#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [25],
-			(CASE WHEN freeItem1='26#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [26],
-			(CASE WHEN freeItem1='27#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [27],
-			(CASE WHEN freeItem1='28#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [28],
-			(CASE WHEN freeItem1='29#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [29],
-			(CASE WHEN freeItem1='30#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [30],
-			(CASE WHEN freeItem1='31#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [31],
-			(CASE WHEN freeItem1='32#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [32],
-			(CASE WHEN freeItem1='33#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [33],
-			(CASE WHEN freeItem1='34#' THEN quantity ELSE 0 END) AS [34],
-			(CASE WHEN freeItem1='35#' THEN quantity ELSE 0 END) AS [35],
-			(CASE WHEN freeItem1='36#' THEN quantity ELSE 0 END) AS [36],
+			specification,freeItem0,name,memo,
+			(CASE WHEN freeItem1='25#' OR freeItem1='XS' THEN quantity ELSE 0 END) AS [25],
+			(CASE WHEN freeItem1='26#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [26],
+			(CASE WHEN freeItem1='27#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [27],
+			(CASE WHEN freeItem1='28#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [28],
+			(CASE WHEN freeItem1='29#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [29],
+			(CASE WHEN freeItem1='30#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [30],
+			(CASE WHEN freeItem1='31#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [31],
+			(CASE WHEN freeItem1='32#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [32],
+			(CASE WHEN freeItem1='33#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [33],
+			(CASE WHEN freeItem1='34#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [34],
+			(CASE WHEN freeItem1='35#' OR freeItem1='7XL' THEN quantity ELSE 0 END) AS [35],
+			(CASE WHEN freeItem1='36#' OR freeItem1='8XL' THEN quantity ELSE 0 END) AS [36],
 			(CASE WHEN freeItem1='37#' THEN quantity ELSE 0 END) AS [37],
 			(CASE WHEN freeItem1='38#' THEN quantity ELSE 0 END) AS [38],
 			(CASE WHEN freeItem1='39#' THEN quantity ELSE 0 END) AS [39],
@@ -130,7 +130,7 @@ FROM(
 			(CASE WHEN freeItem1='44#' THEN quantity ELSE 0 END) AS [44],
 			(CASE WHEN freeItem1='45#' THEN quantity ELSE 0 END) AS [45]
 		FROM(
-			select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity
+			select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity,MAX(d.memo) AS memo
 			from SA_SaleDelivery_b as a 
 			left join aa_unit as b on a.idunit=b.id
 			left join aa_inventory as c on a.idinventory=c.id
@@ -138,9 +138,9 @@ FROM(
 			WHERE d.code='{0}'
 			GROUP BY c.specification,freeItem0,freeItem1,b.name
 		) AS temp
-		GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity
+		GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.memo
 	) AS temp
-	GROUP BY temp.specification, temp.freeItem0,temp.name
+	GROUP BY temp.specification, temp.freeItem0,temp.name,temp.memo
 ) AS temp";
 			return string.Format(sql, Request["code"]);
 		}
@@ -154,31 +154,31 @@ FROM(
 		specification AS cln2,freeItem0 AS cln3, name AS cln4,
 		[28]+[29]+[30]+[31]+[32]+[33]+[34]+[35]+[36]+[37]+[38]+[39]+[40] AS cln5,
 		[28] AS cln6,[29] AS cln7,[30] AS cln8,[31] AS cln9,[32] AS cln10,[33] AS cln11,[34] AS cln12,
-		[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18,price AS cln19
+		[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18,price AS cln19,memo AS cln21
 	FROM(
 		SELECT
-			specification,freeItem0,name,
+			specification,freeItem0,name,memo,
 			SUM([28]) AS [28],SUM([29]) AS [29],SUM([30]) AS [30],SUM([31]) AS [31],SUM([32]) AS [32],SUM([33]) AS [33],SUM([34]) AS [34],
 			SUM([35]) AS [35],SUM([36]) AS [36],SUM([37]) AS [37],SUM([38]) AS [38],SUM([39]) AS [39],SUM([40]) AS [40],price
 		FROM(
 			SELECT
-				specification,freeItem0,name,
-				(CASE WHEN freeItem1='28#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [28],
-				(CASE WHEN freeItem1='29#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [29],
-				(CASE WHEN freeItem1='30#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [30],
-				(CASE WHEN freeItem1='31#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [31],
-				(CASE WHEN freeItem1='32#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [32],
-				(CASE WHEN freeItem1='33#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [33],
-				(CASE WHEN freeItem1='34#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [34],
-				(CASE WHEN freeItem1='35#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [35],
-				(CASE WHEN freeItem1='36#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [36],
-				(CASE WHEN freeItem1='37#' THEN quantity ELSE 0 END) AS [37],
-				(CASE WHEN freeItem1='38#' THEN quantity ELSE 0 END) AS [38],
-				(CASE WHEN freeItem1='39#' THEN quantity ELSE 0 END) AS [39],
+				specification,freeItem0,name,memo,
+				(CASE WHEN freeItem1='28#' OR freeItem1='XS' THEN quantity ELSE 0 END) AS [28],
+				(CASE WHEN freeItem1='29#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [29],
+				(CASE WHEN freeItem1='30#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [30],
+				(CASE WHEN freeItem1='31#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [31],
+				(CASE WHEN freeItem1='32#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [32],
+				(CASE WHEN freeItem1='33#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [33],
+				(CASE WHEN freeItem1='34#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [34],
+				(CASE WHEN freeItem1='35#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [35],
+				(CASE WHEN freeItem1='36#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [36],
+				(CASE WHEN freeItem1='37#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [37],
+				(CASE WHEN freeItem1='38#' OR freeItem1='7XL' THEN quantity ELSE 0 END) AS [38],
+				(CASE WHEN freeItem1='39#' OR freeItem1='8XL' THEN quantity ELSE 0 END) AS [39],
 				(CASE WHEN freeItem1='40#' THEN quantity ELSE 0 END) AS [40],
 				price
 			FROM(
-				select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity, CONVERT(DECIMAL(18,2),taxPrice) AS price
+				select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity, CONVERT(DECIMAL(18,2),taxPrice) AS price,MAX(d.memo) AS memo
 				from SA_SaleDelivery_b as a 
 				left join aa_unit as b on a.idunit=b.id
 				left join aa_inventory as c on a.idinventory=c.id
@@ -186,9 +186,9 @@ FROM(
 				WHERE d.code='{0}'
 				GROUP BY c.specification,freeItem0,freeItem1,b.name, taxPrice
 			) AS temp
-			GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.price
+			GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.price,temp.memo
 		) AS temp
-		GROUP BY temp.specification, temp.freeItem0,temp.name,temp.price
+		GROUP BY temp.specification, temp.freeItem0,temp.name,temp.price,temp.memo
 	) AS temp
 ) AS temp";
 			return string.Format(sql, Request["code"]);
@@ -203,10 +203,10 @@ FROM(
 		[25]+[26]+[27]+[28]+[29]+[30]+[31]+[32]+[33]+[34]+[35]+[36]+[37]+[38]+[39]+[40]+[41]+[42]+[43]+[44]+[45] AS cln5,
 		[28] AS cln6,[29] AS cln7,[30] AS cln8,[31] AS cln9,[32] AS cln10,[33] AS cln11,[34] AS cln12,
 		[35] AS cln13,[36] AS cln14,[37] AS cln15,[38] AS cln16,[39] AS cln17,[40] AS cln18,price AS cln19,
-		[25] as cln21,[26] as cln22,[27] as cln23,[41] as cln24,[42] as cln25,[43] as cln26,[44] as cln27,[45] as cln28
+		[25] as cln21,[26] as cln22,[27] as cln23,[41] as cln24,[42] as cln25,[43] as cln26,[44] as cln27,[45] as cln28,memo AS cln29
 	FROM(
 		SELECT
-			specification,freeItem0,name,
+			specification,freeItem0,name,memo,
 			SUM([25]) AS [25],
 			SUM([26]) AS [26],
 			SUM([27]) AS [27],
@@ -216,19 +216,19 @@ FROM(
 			price
 		FROM(
 			SELECT
-				specification,freeItem0,name,
-				(CASE WHEN freeItem1='25#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [25],
-				(CASE WHEN freeItem1='26#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [26],
-				(CASE WHEN freeItem1='27#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [27],
-				(CASE WHEN freeItem1='28#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [28],
-				(CASE WHEN freeItem1='29#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [29],
-				(CASE WHEN freeItem1='30#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [30],
-				(CASE WHEN freeItem1='31#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [31],
-				(CASE WHEN freeItem1='32#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [32],
-				(CASE WHEN freeItem1='33#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [33],
-				(CASE WHEN freeItem1='34#' THEN quantity ELSE 0 END) AS [34],
-				(CASE WHEN freeItem1='35#' THEN quantity ELSE 0 END) AS [35],
-				(CASE WHEN freeItem1='36#' THEN quantity ELSE 0 END) AS [36],
+				specification,freeItem0,name,memo,
+				(CASE WHEN freeItem1='25#' OR freeItem1='XS' THEN quantity ELSE 0 END) AS [25],
+				(CASE WHEN freeItem1='26#' OR freeItem1='S' THEN quantity ELSE 0 END) AS [26],
+				(CASE WHEN freeItem1='27#' OR freeItem1='M' THEN quantity ELSE 0 END) AS [27],
+				(CASE WHEN freeItem1='28#' OR freeItem1='L' THEN quantity ELSE 0 END) AS [28],
+				(CASE WHEN freeItem1='29#' OR freeItem1='XL' THEN quantity ELSE 0 END) AS [29],
+				(CASE WHEN freeItem1='30#' OR freeItem1='XXL' THEN quantity ELSE 0 END) AS [30],
+				(CASE WHEN freeItem1='31#' OR freeItem1='XXXL' THEN quantity ELSE 0 END) AS [31],
+				(CASE WHEN freeItem1='32#' OR freeItem1='4XL' THEN quantity ELSE 0 END) AS [32],
+				(CASE WHEN freeItem1='33#' OR freeItem1='5XL' THEN quantity ELSE 0 END) AS [33],
+				(CASE WHEN freeItem1='34#' OR freeItem1='6XL' THEN quantity ELSE 0 END) AS [34],
+				(CASE WHEN freeItem1='35#' OR freeItem1='7XL' THEN quantity ELSE 0 END) AS [35],
+				(CASE WHEN freeItem1='36#' OR freeItem1='8XL' THEN quantity ELSE 0 END) AS [36],
 				(CASE WHEN freeItem1='37#' THEN quantity ELSE 0 END) AS [37],
 				(CASE WHEN freeItem1='38#' THEN quantity ELSE 0 END) AS [38],
 				(CASE WHEN freeItem1='39#' THEN quantity ELSE 0 END) AS [39],
@@ -240,7 +240,7 @@ FROM(
 				(CASE WHEN freeItem1='45#' THEN quantity ELSE 0 END) AS [45],
 				price
 			FROM(
-				select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity, CONVERT(DECIMAL(18,2),taxPrice) AS price
+				select c.specification,freeItem0,freeItem1,b.name,CONVERT(INT,SUM(quantity)) AS quantity, CONVERT(DECIMAL(18,2),taxPrice) AS price,MAX(d.memo) AS memo
 				from SA_SaleDelivery_b as a 
 				left join aa_unit as b on a.idunit=b.id
 				left join aa_inventory as c on a.idinventory=c.id
@@ -248,9 +248,9 @@ FROM(
 				WHERE d.code='{0}'
 				GROUP BY c.specification,freeItem0,freeItem1,b.name, taxPrice
 			) AS temp
-			GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.price
+			GROUP BY temp.specification,temp.freeItem0,temp.freeItem1,temp.name,temp.quantity,temp.price,temp.memo
 		) AS temp
-		GROUP BY temp.specification, temp.freeItem0,temp.name,temp.price
+		GROUP BY temp.specification, temp.freeItem0,temp.name,temp.price,temp.memo
 	) AS temp
 ) AS temp";
 			return string.Format(sql, Request["code"]);
